af42da04fbbdaa5d73141b43ffdeb3f4ba28b928
Caymi Ferreira <118706075+caymiferreira@users.noreply.github.com>
2023-08-24 11:15:52 -0300

    criação da função print_lab()

--- labirinto.py
+++ labirinto.py
@@ -27,3 +27,34 @@ def eh_possivel_sair(lab):
             if 0 <= proxima_coluna < len(lab[0]) and 0 <= proxima_linha < len(lab) and lab[proxima_linha][proxima_coluna] == ' ' and proximo not in marcado:
                 fila.append(proximo)
     return False
+
+def print_lab(lab):
+	for i in lab:
+		print("".join(i))
+
+if __name__ == '__main__':
+
+    labirinto = gera_lab(linhas, colunas)
+
+    labirinto1 = [
+['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+[' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#'],
+['#', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', '#'],
+['#', '#', '#', ' ', ' ', '#', ' ', ' ', '#', '#'],
+['#', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' '],
+['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
+
+    labirinto2 = [
+['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+[' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
+['#', '#', ' ', ' ', ' ', '#', '#', ' ', '#', '#'],
+['#', ' ', ' ', '#', '#', '#', '#', ' ', '#', ' '],
+['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
+
+    resultado_esperado1 = True
+
+    print_lab(labirinto2)
+    resultado1 = eh_possivel_sair(labirinto2)
+
+    print(resultado1 == resultado_esperado1)
+    
