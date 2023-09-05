f21ed527636c18deda7f4377bf1340c6f7bd592c
gustavoataidez <luisgustavoataidee@gmail.com>
2023-09-05 18:58:36 -0300

    melhorias e comentário no código

--- labirinto.py
+++ labirinto.py
@@ -1,41 +1,64 @@
-from collections import deque
 from gera_lab import gera_lab, print_lab
 
 linhas = 6
 colunas = 10
-comeca = (1,0)
-termina = (linhas-2,colunas-1)
+comeca = (1, 0)
+termina = (linhas - 2, colunas - 1)
 
 def eh_possivel_sair(lab):
-    pilha = deque([comeca])
+    # Inicializa uma lista como uma pilha com a posição de início
+    pilha = [comeca]
+    # Inicializa um conjunto para rastrear as posições já visitadas
     marcado = set()
 
+    # Enquanto a pilha não estiver vazia, continue explorando o labirinto
     while pilha:
-        atual = pilha.popleft()
+        # O atual vai ser o elemento do topo da pilha (última posição explorada)
+        atual = pilha.pop()
 
+        # Verifica se a posição atual é igual à posição de término
         if atual == termina:
             return True
 
+        # Obtém as coordenadas da posição atual
         linha, coluna = atual
+        # Adiciona a posição atual ao conjunto de posições marcadas como visitadas
         marcado.add(atual)
-        direita = linha, coluna+1
-        abaixo = linha+1, coluna
-        esquerda = linha, coluna-1
-        acima = linha-1, coluna
 
+        # Calcula as coordenadas das posições vizinhas nas direções: direita, abaixo, esquerda e acima
+        direita = linha, coluna + 1
+        abaixo = linha + 1, coluna
+        esquerda = linha, coluna - 1
+        acima = linha - 1, coluna
+
+        # Cria uma lista com as coordenadas das posições vizinhas
         proximos = [direita, abaixo, esquerda, acima]
 
+        # Itera sobre as posições vizinhas
         for proximo in proximos:
             proxima_linha, proxima_coluna = proximo
 
-            if 0 <= proxima_coluna < len(lab[0]) and 0 <= proxima_linha < len(lab) and lab[proxima_linha][proxima_coluna] == ' ' and proximo not in marcado:
+            # Verifica se a próxima posição está dentro dos limites do labirinto,
+            # se é uma posição válida (espaço vazio) e se não foi marcada como visitada
+            if (
+                0 <= proxima_coluna < len(lab[0])
+                and 0 <= proxima_linha < len(lab)
+                and lab[proxima_linha][proxima_coluna] == " "
+                and proximo not in marcado
+            ):
+                # Adiciona a próxima posição à pilha para explorá-la posteriormente
                 pilha.append(proximo)
+
+    # Se o loop terminar e não encontrar uma saída, retorna False
     return False
 
 def print_lab(lab):
-    lab_str = '\n'.join([''.join(row) for row in lab])
-    print(lab_str)
+    for i in lab:
+        # Junta os caracteres em cada linha do labirinto em uma única string
+        # e imprime a linha
+        print("".join(i))
 
+# Verifica se o script está sendo executado como o programa principal
 if __name__ == '__main__':
 
     labirinto = gera_lab(linhas, colunas)
